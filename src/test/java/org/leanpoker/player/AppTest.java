107186cffd61e471aa5e2ecf6354d314a76184c3
Sandor Dojcsak <sandor.dojcsak@evosoft.com>
2020-02-15 13:42:33 +0100

    JUnit init

---
+++ src/test/java/org/leanpoker/player/AppTest.java
@@ -0,0 +1,13 @@
+package org.leanpoker.player;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class AppTest {
+
+    @Test
+    void appHasAGreeting() {
+        assertNotNull("app should have a greeting", "app should have a greeting");
+    }
+}
